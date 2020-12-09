ea2d5fd89e6befa5f1737f3d8016d5630d5ade75
Nixon Vuele <njvuele@utpl.edu.ec>
2020-12-09 10:59:39 -0500

    Archivos

---
+++ Ejemplos/src/ejemplos/Problema3.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplos;
+/**
+ *
+ * @author Nixon
+ */
+public class Problema3 {
+    public static void main(String[]args){
+	int intcontador;
+	String cadenaAsterisco=("*"),cadenaAsteriscos2=("*");
+	System.out.println(cadenaAsterisco);
+	for (intcontador=1;(!(intcontador==10)) ; intcontador++) {
+            cadenaAsterisco=cadenaAsterisco+cadenaAsteriscos2;
+            System.out.println(cadenaAsterisco);
+	}
+    }
+}
+
